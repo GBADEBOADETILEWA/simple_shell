97ea51e09311daa80ef81926b05657934b8617ab
Gbadebo Adetilewa <Gbadeboadetilewa@gmail.com>
2022-11-18 10:46:36 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -95,12 +95,3 @@ int _myhelp(info_t *info)
 		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact 
